2821d6bc6616311e61f05c2671e9fc444aafa6f4
padraicjb <30627496+padraicjb@users.noreply.github.com>
2020-10-15 18:07:36 -0400

    worked on deck class, ensure if it is all right

--- HW2A/cardDeck.h
+++ HW2A/cardDeck.h
@@ -3,14 +3,87 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-#include <algorithm>
+#include <array>
+#include <card.h>
 
 using namespace std; 
 
-class cardDeck 
+//Effectively is just a linked list class 
+class deck 
 {
-// constructor that is passed n and m and init the code object 
-public:
-cardDeck();
 
+private:
+cardDeck *top, *bottom;
+
+public:
+cardDeck head;
+
+//creates an empty deck of cards with the cards in order
+deck()
+{
+top = NULL;
+bottom = NULL;
+}
+//creates an empty deck of cards with the cards in order
+deck(string s)
+{
+top = NULL;
+bottom = NULL;
+string suits [4] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+for(int i = 0; i < 4; i++)
+{
+    string currentsuit = suits[i];
+    for(int j = 13; j >= 1; j--)
+    {
+        this->add_card(card(currentsuit, j));
+    }
+}
+}
+//used to add a given Node
+void add_card(card c)
+{
+cardDeck *temp = new cardDeck;
+temp->Acard = c;
+temp->next = NULL;
+if(top == NULL)
+{
+    top = temp;
+    bottom = temp;
+}
+else
+{
+    bottom->next = temp;
+    bottom = bottom->next;
+}
+}
+
+//overwritten output operator
+friend ostream& operator<<(ostream& dex, deck* d)
+{
+    //h=d->
+    /*
+    void print(cardDeck* h)
+{
+    //kinda cludgy way to print the first one differnt from the rest
+    if(h != NULL){
+        cout<< h->Acard;
+        h = h->next;
+    }
+    while(h != NULL)
+    {
+        cout<< "-->" <<h->Acard;
+        h == h->next;
+    }
+
+}
+    */
+}
+};
+
+//Effectively is just a node but for this card game
+struct cardDeck
+{
+card Acard;
+struct cardDeck *next;
 };
